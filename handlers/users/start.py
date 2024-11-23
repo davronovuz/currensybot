6f6dae3035e602673407edc2ad7b0cdbf375d9c7
davronovuz <davronovtatu@gmail.com>
2024-11-23 10:04:02 +0500

    databaselar qoshildi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,24 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from data.config import ADMINS
 
+from loader import dp,user_db,bot
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    try:
+        telegram_id=message.from_user.id
+        username=message.from_user.username
+        if not user_db.select_user(telegram_id=telegram_id):
+            user_db.add_user(telegram_id=telegram_id,username=username)
+
+            #foydalanuvchilarni sanaymiz
+            count=user_db.count_users()
+            text="Yangi foydalanuvchi qo'shildi\n"
+            text+=f"Bazada {count} ta  foydalanuvchi bor  \n"
+            await bot.send_message(ADMINS[0],text)
+
+        await message.answer("Xush kelibsiz")
+    except Exception as err:
+        print(err)
--- loader.py
+++ loader.py
@@ -1,8 +1,12 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from utils.db_api.users import UserDatabase
 
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+user_db=UserDatabase(path_to_db="data/user.db")
+
+
